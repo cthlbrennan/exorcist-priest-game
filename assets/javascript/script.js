4bc3e6222e95d9e99564ffc09a559096d4391b7f
Cathal Brennan <cthlbrennan@gmail.com>
2024-07-08 14:58:19 +0000

    Add explanatory comment to script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,6 @@
+//landing page
+
+// this function runs when the How to Play button on the landing page is clicked on/touched. 
 function showRules(){
     let rulesBox = document.getElementById("rules-box");
     let animationBox = document.getElementById("intro-animation-box")
